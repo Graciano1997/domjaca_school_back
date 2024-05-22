ae393cf29cdf8ec07b05fc8cf8ec67dc5d3d1e11
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-05-23 00:06:48 +0200

    update the htm files folder

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app=http.createServer((req,res)=>{
     let page=routes(req.url);
 
     if(page!==undefined){
-    res.statusCode=200;
+    res.writeHead(200,{'Content-Type':'text/html'})
         res.end(page);
     }else{
-        res.statusCode=404;
-        page=pageFinder("./view/404.html")
+        res.writeHead(404,{'Content-Type':'text/html'});
+        page=pageFinder("./public/404.html")
         res.end(page);
     }
 });
 
-app.listen(3000,'localhost',()=>{console.log("Servidor Dom Jaca Ligado!")});
+app.listen(3000,'localhost',()=>{console.log("Servidor da Escola Dom Jaca Ligado!")});
