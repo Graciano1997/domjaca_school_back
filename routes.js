a9ccaf67ab3888c125b73ca3ac5630ca1f264a2c
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-05-24 23:57:44 +0200

    add the student platform

--- routes.js
+++ routes.js
@@ -16,6 +16,20 @@ const routes = (route) => {
         case '/acerca':
             page = pageFinder("./view/acerca.html");
             break;
+        case '/news':
+        case '/anuncios':
+            page = pageFinder("./view/anuncio.html");
+            break;
+        case '/estudantes_honra':
+            page = pageFinder("./view/estudanteHonra.html");
+            break;
+        case '/novas_matriculas_confirmacoes':
+            page = pageFinder("./view/matricula.html");
+            break;
+        case '/portal_estudante':
+        case '/student_gate':
+            page = pageFinder("./view/portal.html");
+            break;
     }
     console.log(route);
     return page;
