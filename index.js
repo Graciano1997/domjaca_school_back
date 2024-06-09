54e796f1b6330d886a279f3eb30f4fde7b24da1e
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-06-09 19:14:06 -0400

    increase the header hight

--- index.js
+++ index.js
@@ -1,119 +1,56 @@
 import express from "express";
+// import path, { dirname } from "path";
+import pageFinder from "./module/pageFinder.js";
 
 const app=express();
 
 app.use(express.static('public'));
-app.use(express.static('view'));
 
 app.get('/acerca',(request,response)=>{
-  response.status(200).sendFile("/404.html");
+  const page=pageFinder('./view/acerca.html');
+  response.status(200).end(page);
+});
+
+app.get('/anuncios',(request,response)=>{
+  const page=pageFinder('./view/anuncio.html');
+   response.status(200).end(page);
+});
+
+app.get('/estudantes_honra',(request,response)=>{
+  const page=pageFinder('./view/estudanteHonra.html');
+   response.status(200).end(page);
+});
+
+app.get('/novas_matriculas_confirmacoes',(request,response)=>{
+  const page=pageFinder('./view/matricula.html');
+   response.status(200).end(page);
+});
+
+app.get('/portal_estudante',(request,response)=>{
+  const page=pageFinder('./view/portal.html');
+   response.status(200).end(page);
+});
+
+app.get('/alums',(request,response)=>{
+  const page=pageFinder('./view/alums.html');
+   response.status(200).end(page);
 });
 
 app.get('/',(request,response)=>{
-   response.status(200).sendFile("/index.html");
+  const page=pageFinder('./view/index.html');
+   response.status(200).end(page);
 });
+
+app.use((request,response,next)=>{
+  const page=pageFinder('./view/404.html');
+   response.status(200).end(page);
+})
 
 app.listen(3000,"0.0.0.0",()=>{
-  console.log("**************************************");
-  console.log("*                                    *");
-  console.log("* Servidor da Escola Dom Jaca Ligado!*");
-  console.log("*                                    *");
-  console.log("**************************************");
+  console.log("***************************************");
+  console.log("*                                     *");
+  console.log("* Servidor da Escola Dom Jaca Ligado! *");
+  console.log("*                                     *");
+  console.log("***************************************");
 });
 
-
-// app.get('/',(request,response)=>{
-//   return response.status(200).sendFile("/index.html");
-// });
-// app.get('/',(request,response)=>{
-//   return response.status(200).sendFile("/index.html");
-// });
-// app.get('/',(request,response)=>{
-//   return response.status(200).sendFile("/index.html");
-// });
-// app.get('/',(request,response)=>{
-//   return response.status(200).sendFile("/index.html");
-// });
-// app.get('/',(request,response)=>{
-//   return response.status(200).sendFile("/index.html");
-// });
-
-
-// case '/':
-//   case '/home':
-//       page = pageFinder("./view/index.html");
-//       break;
-//   case '/about':
-//   case '/acerca':
-//       page = pageFinder("./view/acerca.html");
-//       break;
-//   case '/news':
-//   case '/anuncios':
-//       page = pageFinder("./view/anuncio.html");
-//       break;
-//   case '/estudantes_honra':
-//       page = pageFinder("./view/estudanteHonra.html");
-//       break;
-//   case '/novas_matriculas_confirmacoes':
-//       page = pageFinder("./view/matricula.html");
-//       break;
-//   case '/portal_estudante':
-//   case '/student_gate':
-//       page = pageFinder("./view/portal.html");
-//       break;
-//   case '/alums':
-//       page = pageFinder("./view/alums.html");
-//       break;
-//   case '/login':
-//       page = pageFinder("./view/dashboard/login.html");
-//       break;
-//   case '/dashboard':
-//       page = pageFinder("./view/dashboard/dashboard.html");
-//       break;
-//   case '/dashboard/perfil':
-//       page = pageFinder("./view/dashboard/perfil.html");
-//       break;
-//   case '/dashboard/aproveitamento':
-//       page = pageFinder("./view/dashboard/aproveitamento.html");
-//       break;
-//   case '/dashboard/notas':
-//       page = pageFinder("./view/dashboard/notas.html");
-//       break;
-
-
-// import http from 'node:http';
-
-// const app = http.createServer((req, res) => {
-//   if (req.url.indexOf('.css') != -1) {
-//     const cssSheet = pageFinder('./public/assets/style.css');
-//     res.writeHead(200, { 'Content-Type': 'text/css' });
-//     res.write(cssSheet.toString());
-//   }
-
-//   if (req.url.indexOf('.js') != -1) {
-//     const jsPage = pageFinder('./public/assets/index.js');
-//     res.writeHead(200, { 'Content-Type': 'text/javascript' });
-//     res.write(jsPage.toString());
-//   }
-
-//     let page = routes(req.url);
-//     if ((page !== undefined) && (page !== '')) {
-//       res.writeHead(200, { 'Content-Type': 'text/html' });
-//       res.write(page);
-//       res.end();
-//     }
-
-//     if(page===undefined){
-//       page = pageFinder("./view/404.html");
-//       res.statusCode=404;
-//       res.statusMessage="The Page Was not found!";
-//       res.write(page);
-//       res.end();
-//     }
-
-//     if(page===''){
-//       res.end();
-//     }
-// });
-
-// app.listen(3000, 'localhost', () => {console.log("Servidor da Escola Dom Jaca Ligado!");});
--- public/assets/index.js
+++ public/assets/index.js
@@ -1,3 +1,11 @@
+const elementLinks=document.querySelectorAll('.menuItem ul li');
+
+elementLinks.forEach((element)=>{
+     element.addEventListener("click",(el)=>{
+        location.href=element.firstChild.href;
+     })
+ })
+
 document.querySelector('#btnHamburguer').addEventListener("click",(el)=>{
 document.querySelector('.menuContainer').classList.add('show');
 document.querySelector('#body').classList.add("noScrool");
