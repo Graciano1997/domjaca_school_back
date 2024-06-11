b41e16b5331cfd6b038bbd0c03588466daa7eade
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-06-11 13:42:47 -0400

    add the responsivity in the student pontuation area

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,5 +1,23 @@
 const elementLinks=document.querySelectorAll('.menuItem ul li');
+const portalAvatarContainerImage = document.querySelector('.portalAvatarContainer img');
+const seePontuationBtns=document.querySelectorAll('.seeMarks');
+if(seePontuationBtns!==null){
+   seePontuationBtns.forEach((btn)=>{
+      btn.addEventListener("click",(el)=>{
+         const grandParent=el.target.parentNode.parentNode;
+         const pontuationContainer=grandParent.querySelector('.pontuationContainer');
+         pontuationContainer.classList.toggle('seePontuationContainer');
+         el.target.textContent=pontuationContainer.classList.contains('seePontuationContainer')?'ocultar':'ver';
+      });
+   })
+}
 
+if(portalAvatarContainerImage!==null){
+   portalAvatarContainerImage.addEventListener("click",()=>{
+      window.location.href="/portal/perfil";
+   })
+
+}
 elementLinks.forEach((element)=>{
      element.addEventListener("click",(el)=>{
         location.href=element.firstChild.href;
