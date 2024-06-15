0f6c3803c3920e6b6d99fc3a156e46bcc0070a77
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-06-15 09:39:44 -0400

    set the EJS template

--- Routes/portal.js
+++ Routes/portal.js
@@ -1,25 +1,21 @@
 const express=require('express');
-const pageFinder = require('../module/pageFinder');
 const router=express.Router();
 router.use(express.static('public'));
 
 router.get('/',(request,response)=>{
-    const page=pageFinder('./view/dashboard/dashboard.html');
-     response.status(200).end(page);
+    response.render('./dashboard/dashboard');
+    
   });
 
 router.get('/notas',(request,response)=>{
-    const page=pageFinder('./view/dashboard/notas.html');
-     response.status(200).end(page);
+     response.render('./dashboard/notas');
 });
 router.get('/aproveitamento',(request,response)=>{
-    const page=pageFinder('./view/dashboard/aproveitamento.html');
-     response.status(200).end(page);
+     response.render('./dashboard/aproveitamento');
 });
 
 router.get('/perfil',(request,response)=>{
-    const page=pageFinder('./view/dashboard/perfil.html');
-     response.status(200).end(page);
+     response.render('./dashboard/perfil');
 });
 
 module.exports=router;
--- index.js
+++ index.js
@@ -3,53 +3,57 @@ const pageFinder=require("./module/pageFinder");
 const app=express();
 
 app.use(express.static('public'));
+app.set('view engine','ejs');
 
 
 app.get('/acerca',(request,response)=>{
-  const page=pageFinder('./view/acerca.html');
-  response.status(200).end(page);
+  // const page=pageFinder('./view/acerca.html');
+  response.render('acerca');
+  // response.status(200).end(page);
 });
 
 app.get('/anuncios',(request,response)=>{
-  const page=pageFinder('./view/anuncio.html');
-   response.status(200).end(page);
+  response.render('anuncio');
 });
 
 app.get('/estudantes_honra',(request,response)=>{
-  const page=pageFinder('./view/estudanteHonra.html');
-   response.status(200).end(page);
+  // const page=pageFinder('./view/estudanteHonra.html');
+  //  response.status(200).end(page);
+   response.render('estudanteHonra');
 });
 
 app.get('/novas_matriculas_confirmacoes',(request,response)=>{
-  const page=pageFinder('./view/matricula.html');
-   response.status(200).end(page);
+  // const page=pageFinder('./view/matricula.html');
+  //  response.status(200).end(page);
+   response.render('matricula');
 });
 
 app.get('/portal_estudante',(request,response)=>{
-  const page=pageFinder('./view/portal.html');
-   response.status(200).end(page);
+  // const page=pageFinder('./view/portal.html');
+   response.render('portal');
 });
 
 app.get('/alums',(request,response)=>{
-  const page=pageFinder('./view/alums.html');
-   response.status(200).end(page);
+  // const page=pageFinder('./view/alums.html');
+   response.render('alums');
 });
 
 app.get('/',(request,response)=>{
-  const page=pageFinder('./view/index.html');
-   response.status(200).end(page);
+  // const page=pageFinder('./view/index.html');
+    // response.status(200).end(page);
+   response.render('index');
 });
 
 app.get('/login',(request,response)=>{
-  const page=pageFinder('./view/dashboard/login.html');
-   response.status(200).end(page);
+  response.render('login');
 });
 
 app.use('/portal',require('./Routes/portal'));
 
 app.use((request,response,next)=>{
-  const page=pageFinder('./view/404.html');
-   response.status(404).end(page);
+  // const page=pageFinder('./view/404.html');
+   response.status(404);
+   response.render('404')
 })
 
 app.listen(3000,"0.0.0.0",()=>{
