df4debb992c7285b8d5c3163ea40de3ad4f0c588
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-08-28 21:34:24 -0400

    update the visao img

--- Routes/portal.js
+++ Routes/portal.js
@@ -7,45 +7,25 @@ router.use(express.static('public'));
 // Route to render the dashboard main page
 router.get('/', (req, res) => {
      userNavegatingShow(req);
-    res.render('./dashboard/dashboard', (err) => {
-        if (err) {
-            console.error('Error rendering dashboard:', err);
-            res.status(500).send('Server Error');
-        }
-    });
+    res.render('./dashboard/dashboard');
 });
 
 // Route to render the 'notas' page
 router.get('/notas', (req, res) => {
      userNavegatingShow(req);
-    res.render('./dashboard/notas', (err) => {
-        if (err) {
-            console.error('Error rendering notas:', err);
-            res.status(500).send('Server Error');
-        }
-    });
+    res.render('./dashboard/notas');
 });
 
 // Route to render the 'aproveitamento' page
 router.get('/aproveitamento', (req, res) => {
      userNavegatingShow(req);
-    res.render('./dashboard/aproveitamento', (err) => {
-        if (err) {
-            console.error('Error rendering aproveitamento:', err);
-            res.status(500).send('Server Error');
-        }
-    });
+    res.render('./dashboard/aproveitamento');
 });
 
 // Route to render the 'perfil' page
 router.get('/perfil', (req, res) => {
      userNavegatingShow(req);
-     response.status(200).res.render('./dashboard/perfil', (err) => {
-        if (err) {
-            console.error('Error rendering perfil:', err);
-            res.status(500).send('Server Error');
-        }
-    });
+    res.render('./dashboard/perfil');
 });
 
 module.exports = router;
